23f43dc8ceb51eba44c0d3d7612c09c8e5809fbf
mLachaw <michallachawiec@gmail.com>
2020-01-27 15:24:28 +0100

    [2019-11-11] Challenge #380

---
+++ src/challenge/morsecode/MorseCode.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package challenge.morsecode;
+
+import java.awt.EventQueue;
+
+/**
+ *
+ * @author MLachaw
+ */
+
+
+public class MorseCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable()
+                {
+                    
+                    @Override
+                    public void run()
+                    {
+                        //new Ramka();
+                        new Ramka().setVisible(true);
+                    }
+                });
+    }
+    
+}
